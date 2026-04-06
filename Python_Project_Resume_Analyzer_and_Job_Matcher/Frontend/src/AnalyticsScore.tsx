62b8cc7f4af8c5e83f3cca52814fea68db7fbc7b
Sairam <jrsairam5686@gmail.com>
2026-04-06 20:11:46 +0530

    --resume analyzer completed

--- Python_Project_Resume_Analyzer_and_Job_Matcher/Frontend/src/AnalyticsScore.tsx
+++ Python_Project_Resume_Analyzer_and_Job_Matcher/Frontend/src/AnalyticsScore.tsx
@@ -1,55 +1,122 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 
 const AnalyticsScore = () => {
 
   
-
+  const [Ratings, setRatings] = useState("")
   const { state: result } = useLocation();
   console.log(result);
+useEffect(() => {
+  
+  if(result.Score >= 90){
+    setRatings("Look Alright")
+  }
+  else if(result.Score > 60){
+    setRatings("Look Alright")
+  }
+  else{
+    setRatings("Need Improvement")
+
+  }
+
+}, [])
+
+            
   return (
-   
-    <div className='h-screen w-full flex flex-col items-center pt-10 pr-10 pl-10'>
+    <div className='max-w-4xl mx-auto px-6 py-14'>
+      <div>
+        <p className='font-mono font-light text-gray-400'>
+          Resume Analysis
+        </p>
+        <h1 className='text-5xl font-semibold'>
+          Your Matching  <span className='text-emerald-400'> Score </span>
+        </h1>
+      </div>
 
-        <div>      
-        <h1 className='text-3xl font-semibold'> Result Analytics From your resume </h1>
-        </div>
+      <div className='border-1 border-gray-200 mt-4'></div>
 
-        <div className='h-[500px] w-full flex flex-row gap-10  mt-20 '>
+      <div className='flex flex-row gap-10'>
 
-
-          <div className='h-[500px] w-[49%] bg-gray-200  rounded-2xl'>
-            <div className='flex flex-col  '>
-              <h1 className='text-2xl font-semibold text-center mt-10 mb-5'>Matched Words</h1>
-              <div className='flex flex-row gap-2 flex-wrap'>
+      {/* for matchers indicators */}
+      <div className='flex flex-col  boder-1  mt-5  w-[75%] border-black'>
+        
+        
+        
+        <div className='flex flex-col bg-gray-200 rounded-xl px-5 py-4'> 
+            <div className='flex flex-row mb-4 justify-between items-center'>
+              <p className='font-bold text-gray-600 font-mono'>Matching Words</p>
+              <p className='font-mono font-bold   px-2 py-1 bg-emerald-900 text-emerald-200 rounded-3xl text-center'>found {result?.Matching_words?.length}</p>
+            </div>
+            <div className='border-1 border-gray-300 mt-4 mb-3'></div>
+              
+              <div className=' flex flex-row flex-wrap gap-2 text-balance'>
               {result?.Matching_words?.map((word: string, index: number) => {
-              return ( <p key={index} className="text-md bg-emerald-500 rounded-xl p-2 ">{word} </p>  );
-            })}
+              return (
+                 <p key={index}  className="font-mono font-bold   px-2 py-1 bg-emerald-900 text-emerald-200 rounded-3xl text-center">{word}</p>);})}
             </div>
-            </div>
+        </div>
+     
+     
+     
 
-            <div className='flex flex-col '>
-              <h1 className='text-2xl font-semibold text-center mt-10 mb-3'>Missing Words</h1>
-              <div className='flex flex-row gap-2 flex-wrap'>
+          
+        <div className='flex flex-col bg-gray-200 rounded-xl mt-5 px-5 py-4'> 
+            <div className='flex flex-row mb-4 justify-between items-center'>
+              <p className='font-bold text-gray-600 font-mono'>Missing Words</p>
+              <p className='font-mono font-bold   px-2 py-1 bg-red-900 text-red-200 rounded-3xl text-center'>found {result?.Missing_words?.length}</p>
+            </div>
+            <div className='border-1 border-gray-300 mt-4 mb-3'></div>
+              
+              <div className=' flex flex-row flex-wrap gap-2 text-balance'>
               {result?.Missing_words?.map((word: string, index: number) => {
               return (
-                <p key={index} className="text-md bg-red-500 rounded-xl p-2 ">{word} </p>);})}
-              </div>
-
+                 <p key={index}  className="font-mono font-bold   px-2 py-1 bg-red-900 text-red-200 rounded-3xl text-center">{word}</p>);})}
             </div>
-          </div>
-
-
-
-
-          <div className='h-[500px] w-[49%] bg-amber-200  rounded-2xl'>
-            <h1 className='text-4xl font-semibold text-center mt-10 '>Score</h1>
-            <div className='flex mt-10 justify-center h-full'>
-              <p className='text-4xl font-semibold'>{result?.Score}% out of 100%</p>
-         
-            </div>
-          </div>
         </div>
+
+     
+      </div>
+
+
+
+
+
+      
+        
+      <div className='bg-gray-200 items-center w-[40%] flex flex-col mt-5 rounded-2xl'>
+              
+                <p className='font-mono mt-20 font-bold text-gray-600'>
+                  Your Matching Score
+                </p>
+
+
+                  <div className="relative size-40 mt-10">
+                      <svg className="rotate-[135deg] size-full" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+                      
+                        <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-foreground/10" stroke-width="1.5" stroke-dasharray={`${(result?.Score ?? 0) / 100 * 75} 100`} stroke-linecap="round"></circle>
+
+                      
+                        <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-blue-400" stroke-width="1.5" stroke-dasharray="37.5 100" stroke-linecap="round"></circle>
+                      </svg>
+
+
+                      <div className="absolute top-1/2 start-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                        <span className="text-4xl font-bold text-blue-400">{result?.Score}</span>
+                        <span className="text-primary block">Score</span>
+                      </div>
+                    </div>
+
+
+                    <h3 className='text-2xl text-blue-400 font-mono font-semibold'>
+                    {Ratings}
+                    </h3>
+
+      </div>
+
+
+
+      </div>
 
     </div>
   )
