f011273dfc50d5b915152787433710472f93b98c
Sairam <jrsairam5686@gmail.com>
2026-04-10 20:40:29 +0530

    --jar sql

--- Java_Project_Library_Management_System/Library_Management_System/src/App.java
+++ Java_Project_Library_Management_System/Library_Management_System/src/App.java
@@ -1,5 +1,12 @@
+import java.sql.*;
+import java.util.*;
+
+// Library_management_system -- connection name
+// lms -- database name
+// users , books , borrowlist - tables name
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+       
     }
 }
